17b723d8d0e535146bb7e96570dbcd860ef99464
Justin R. Merwin <hello@gatheredroots.net>
2025-07-12 20:17:05 -0700

    Mobile navbar again..

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'; // For hamburger and close icons
 
